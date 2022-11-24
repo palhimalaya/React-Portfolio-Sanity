a119aa3c8ff6c56568b9d81c1be64a7933f8ceaa
Himalaya Pal <45174860+palhimalaya@users.noreply.github.com>
2022-11-24 08:07:43 +0545

    finished portfolio

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
+
+import React from "react";
+
+const NavigationDots = ({ active }) => {
+  return (
+    <div className="app__navigation">
+      {["home", "about", "work", "skills", "testimonials", "contact"].map(
+        (item, index) => (
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={active === item ? { backgroundColor: "#313BAC" } : {}}
+          />
+        )
+      )}
+    </div>
+  );
+};
+
+export default NavigationDots;
